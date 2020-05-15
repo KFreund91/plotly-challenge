d7d6adfe015efb357df797d3e648cbed6e2315be
Kaylee Freund <kbeth91@gmail.com>
2020-05-15 16:15:08 -0400

    hard coded for first id

--- app.js
+++ app.js
@@ -1,7 +1,74 @@
-// Use the d3 library to load in samples.json
-d3.json("../../samples.json", function(data) {
-  console.log(data[0]);
-});
+  //bar chart
+  d3.json("samples.json").then(sampledata => {
+    //console.log(sampledata.samples)
+    var OTU_top = sampledata.samples[0].otu_ids;
+    //console.log(OTU_top);
+    var ids = OTU_top.map(d => "OTU " + d);
+    var sampleValues = sampledata.samples[0].sample_values;
+    //console.log(sampleValues);
+    var otuLabels = sampledata.samples[0].otu_labels;
+    //console.log(otuLabels);
+
+    var barchart = [
+      {
+        x: sampleValues.slice(0, 10).reverse(),
+        y: ids,
+        text: otuLabels.slice(0, 10),
+        marker: {
+          color: 'blue'
+        },
+        type: "bar",
+        orientation: "h",
+      }]
+    var layout = {
+      title: "Top 10 OTU",
+      yaxis: { title: "OTU IDs" },
+      xaxis: { title: "Sample Values" }
+    }
+    Plotly.newPlot("bar", barchart, layout);
+  });
+
+  //bubble chart
+  d3.json("samples.json").then(sampledata => {
+    var OTU_top = sampledata.samples[0].otu_ids;
+    var sampleValues = sampledata.samples[0].sample_values;
+    var otuLabels = sampledata.samples[0].otu_labels;
+
+    var bubble = {
+      x: OTU_top,
+      y: sampleValues,
+      text: otuLabels,
+      mode: `markers`,
+      marker: {
+        size: sampleValues
+      }
+    };
+
+    var data = [bubble];
+    var layout = {
+      title: "Belly Button Bacteria",
+      xaxis: { title: "OTU ID" }
+    };
+    Plotly.newPlot("bubble", data, layout);
+  })
+
+
+//demographics
+
+  d3.json("samples.json").then(sampledata => {
+    var sampleData = d3.select(`#sample-metadata`);
+    Object.entries(sampledata.metadata[0]).forEach(function ([key, value]) {
+      var row = sampleData.append("p");
+      row.text(`${key}:${value}`)
+    })
+  })
+
+
+
+
+
+
+
 
 
 
